8898dca246ebaa0c71dc8d23bfc23aca82153a74
AnaCouto1409 <35375044+AnaCouto1409@users.noreply.github.com>
2022-01-19 17:47:29 -0300

    Add files via upload

---
+++ aula aplicações/index.js
@@ -0,0 +1,13 @@
+///DEUS CUIDA DEMIM
+function love(){
+    document.getElementById("love2").style.background="#7e459b";
+    document.getElementById("love2").style.width="220px";
+    document.getElementById("love2").style.height="220px";
+    document.getElementById("love2").style.border="20px";
+    document.getElementById("love2").style.color="white";
+    var titulo= document.querySelector("h1");
+    titulo.innerText ="imprime ai para mim please";
+}
+
+
+
